6f2c45a9b00fea03542a404ee50ebb2abff8cbf8
Laci Szucs <ifrozen@gmail.com>
2016-10-03 16:18:57 +0200

    chore(world): config and container mocks

---
+++ features/world.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function() {
+  this.World = World;
+};
+
+function World() {
+  const config = Object.freeze({
+    get: key => {
+      switch (key) {
+        case 'httpServer.type': return 'memory';
+        // case 'httpServer.type': return 'koa';
+        default: throw new Error(`Could not find config value for ${key}`)
+      }
+      return key === 'httpServer.type' ? 'memory' : undefined;
+    }
+  });
+  let containerObject = {
+    config,
+    httpServer: require('../index')(config)
+};
+  this.container = Object.freeze({
+    get(key) {
+      return containerObject[key];
+    }
+  });
+}
