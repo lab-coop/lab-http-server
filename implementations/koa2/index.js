be3dae005cc8ea3cf0aa4cfdd9e5841630b06887
Laci Szucs <ifrozen@gmail.com>
2016-10-18 01:00:29 +0200

    feat(createServer): createServer accepts a serverUrl argument

--- implementations/koa2/index.js
+++ implementations/koa2/index.js
@@ -4,14 +4,17 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyparser = require('koa-bodyparser');
 const fetch = require('node-fetch');
+const url = require('url');
 
 module.exports = function HTTPServerKoaImplementation() {
   return Object.freeze({
     createServer
   });
-  function createServer() {
+
+  function createServer(serverUrl='http://localhost:8080') {
     const App = new Koa(), router = new Router();
-    let server, hostname = 'localhost', port = 12345;
+    const {protocol, host, port} = url.parse(serverUrl);
+    let server;
     return Object.freeze({
       registerRoute,
       start,
@@ -38,8 +41,7 @@ module.exports = function HTTPServerKoaImplementation() {
     }
 
     function sendRequest(method, query, {body, headers}={}) {
-      if (query[0] === '/') query = query.slice(1);
-      return fetch(`http://${hostname}:${port}/${query}`, {
+      return fetch(`${protocol}//${host}${query}`, {
         method, body, headers
       }).then(response => {
         return response.text().then(responseText => {
