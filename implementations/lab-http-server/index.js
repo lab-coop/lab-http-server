263c549d632aa02939062e1f81fde5f5bf8fd74f
Laci Szucs <ifrozen@gmail.com>
2016-10-23 16:27:08 +0200

    refactor(di): use container.getImplementation

--- implementations/lab-http-server/index.js
+++ implementations/lab-http-server/index.js
@@ -2,7 +2,7 @@
 
 module.exports = function LabHTTPServerFactory(container) {
   const implementationType = container['lab-config'].get('httpServer.type');
-  return container[`lab-http-server-${implementationType}`];
+  return container.getImplementation(`lab-http-server`, implementationType);
 };
 
 module.exports.type = 'factory';
