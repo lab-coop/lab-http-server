9d01ffa3e7d74f4a4b43e2d8ce0f4dff8996468b
Laci Szucs <ifrozen@gmail.com>
2016-11-20 14:20:11 +0100

    chore(di): updated lab-di and added lab-config as a dependency
    - lab-di signature change: Use registerDir from lab-di/tools
    - lab-config extracted: Use lab-config instead of mocking in this package.

--- features/world.js
+++ features/world.js
@@ -1,5 +1,5 @@
 'use strict';
-const di = require('lab-di');
+const diTools = require('lab-di/tools');
 const path = require('path');
 
 module.exports = function() {
@@ -7,15 +7,16 @@ module.exports = function() {
 };
 
 function World() {
-  this.container = di();
-  this.container.registerDir(path.join(__dirname, '../dependencies'));
+  const diWrapper = diTools();
+
+  require('../index')(diWrapper);
+
+  this.container = diWrapper.getDI();
+  this.container.registerModule(require('lab-config'), 'lab-config'); 
 
   const config = this.container.get('lab-config');
   // config.update('httpServer.type', process.env.LAB_HTTP_SERVER_TYPE || 'memory');
   // config.update('httpClient.type', process.env.LAB_HTTP_CLIENT_TYPE || 'server');
   config.update('httpServer.type', process.env.LAB_HTTP_SERVER_TYPE || 'koa2');
   config.update('httpClient.type', process.env.LAB_HTTP_CLIENT_TYPE || 'fetch');
-
-
-  require('../index')(this.container);
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 const path = require('path');
-
-module.exports = container => {
-  container.registerDir(path.join(__dirname, '/implementations'));
+module.exports = (di) => {
+  di.registerDir(path.join(__dirname, '/implementations'));
 };
