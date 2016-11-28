c65de981ad5971fba385701a64b35f7659079b83
Laci Szucs <ifrozen@gmail.com>
2016-11-28 12:02:51 +0100

    chore(config): use newest lab-config signature
    Please note that I had to register the memory implementation separately.

--- features/world.js
+++ features/world.js
@@ -13,11 +13,12 @@ function World() {
 
   this.container = diWrapper.getDI();
   this.container.registerModule(require('lab-config'), 'lab-config');
-  attachHttpClient(this.container);
-
+  this.container.registerModule(require('lab-config/implementations/memory'), 'lab-config-memory');
   const config = this.container.get('lab-config');
   config.update('httpServer.type', process.env.LAB_HTTP_SERVER_TYPE || 'memory');
   config.update('httpClient.type', process.env.LAB_HTTP_CLIENT_TYPE || 'memory');
   // config.update('httpServer.type', process.env.LAB_HTTP_SERVER_TYPE || 'koa2');
   // config.update('httpClient.type', process.env.LAB_HTTP_CLIENT_TYPE || 'fetch');
+
+  attachHttpClient(this.container);
 }
